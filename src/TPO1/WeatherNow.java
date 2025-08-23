5a6e1e7010355d31fd8ed824c825724b6246d8e1
Jere <jeremias.torres@est.fi.uncoma.edu.ar>
2025-08-22 21:57:11 -0300

    Hice que el metodo obtenerAltura devuelva un tipo Int

--- src/TPO1/WeatherNow.java
+++ src/TPO1/WeatherNow.java
@@ -1,18 +1,26 @@
 package TPO1;
 
-import java.net.http.*;
 import java.net.URI;
+import java.net.http.HttpClient;
+import java.net.http.HttpRequest;
+import java.net.http.HttpResponse;
 
 public class WeatherNow {
-    public static String get(String city) throws Exception {
-        var client = HttpClient.newHttpClient();
-        var req = HttpRequest.newBuilder(URI.create("https://wttr.in/" + city + "?format=3")).build();
+    public static int obtenerTemperatura(String ciudad) throws Exception {
+        int temperatura;
+        HttpClient cliente = HttpClient.newHttpClient();
+        HttpRequest solicitud = HttpRequest.newBuilder(
+                URI.create("https://wttr.in/" + ciudad + "?format=3")
+        ).build();
 
-        return client.send(req, HttpResponse.BodyHandlers.ofString()).body();
-
+        String cuerpo = cliente.send(solicitud, HttpResponse.BodyHandlers.ofString()).body(); // "Salta: 🌦 +16°C"
+        String ultimo = cuerpo.trim().substring(cuerpo.lastIndexOf(' ') + 1);                 // "+16°C"
+        temperatura = Integer.parseInt(ultimo.replaceAll("[^0-9+-]", "").trim());            // 16
+        return temperatura;
     }
 
+
     public static void main(String[] args) throws Exception {
-        System.out.println(get("Salta"));
+        System.out.println(obtenerTemperatura("Salta")); // solo el número
     }
 }
